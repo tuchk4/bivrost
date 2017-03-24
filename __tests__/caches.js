199a7be7e4668b5a9fc468274be3b81569d14521
Maksym Bugai <m.bugai@plarium.com>
2017-03-24 14:50:57 +0200

    add default cache

--- __tests__/caches.js
+++ __tests__/caches.js
@@ -2,6 +2,49 @@ import DataSource from '../src/data/source';
 
 
 describe('Cache', () => {
+
+  it('default cache', () => {
+    class DS1 extends DataSource {
+      static defaultCache = {
+        isGlobal: true,
+        ttl: 1
+      };
+
+      static cache = {
+        foo: {enabled: true}
+      }
+    }
+
+    class DS2 extends DS1 {
+      static defaultCache = {
+        ttl: 10
+      };
+    }
+
+
+    let ds2 = new DS2();
+    expect(ds2.getCache('foo').ttl).toBe(10);
+
+    let ds1 = new DS1();
+    expect(ds1.getCache('foo').ttl).toBe(1);
+
+    expect(ds1.constructor.defaultCache).toEqual({
+      ttl: 1,
+      isGlobal: true,
+    });
+    expect(DS1.defaultCache).toEqual({
+      ttl: 1,
+      isGlobal: true,
+    });
+
+    expect(ds2.constructor.defaultCache).toEqual({
+      ttl: 10,
+    });
+    expect(DS2.defaultCache).toEqual({
+      ttl: 10,
+    });
+  });
+
   it('should be able to work per-class', () => {
     class DS extends DataSource {
       static cache = {
